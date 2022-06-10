3baca6e470e1dda3c8212cc7f7601cc995c66928
Jonathan Fontes <jonathan.alexey@icloud.com>
2022-06-10 10:52:59 +0100

    FIX: PHPUnit isn't match with PHP Version

--- migrations-db.php
+++ migrations-db.php
@@ -1,13 +1,15 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 use Doctrine\DBAL\Connection;
 use Gravatalonga\Framework\ValueObject\Path;
 use Gravatalonga\KingFoundation\Kernel;
+use function Gravatalonga\Framework\container;
 
 $app = new Kernel(new Path(__DIR__));
 
 $container = container();
 
+/**
+ * @var Connection
+ */
 return $container->get(Connection::class);
